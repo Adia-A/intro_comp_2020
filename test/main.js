136ce59c9112d31c4cbd36a751bc4e816fbb77b2
Adia Alderson <adia.alderson@slalom.com>
2020-08-11 21:44:08 -0500

    story works

--- test/main.js
+++ test/main.js
@@ -1,72 +1,112 @@
 (function (storyContent) {
+  /** ---------------------------
+   *  Helper Functions
+   * ----------------------------
+   */
 
-    /** ---------------------------
-     *  Helper Functions
-     * ----------------------------
-     */
+  const helper = {
+    parseGlobalTags: (tags) => {
+      let tagsObject = {};
+      if (tags) {
+        tags.forEach((tag) => {
+          let tagArray = tag.split(":");
+          if (tagArray.length === 2) {
+            tagsObject[`${tagArray[0]}`] = tagArray[1].trim();
+          }
+        });
+      } else {
+        return null;
+      }
 
-    const helper = {
-        parseGlobalTags: (tags) => {
-            let tagsObject = {}
-            if (tags) {
-                tags.forEach(tag => {
-                    let tagArray = tag.split(':')
-                    if(tagArray.length === 2) {
-                        tagsObject[`${tagArray[0]}`] = tagArray[1].trim()
-                    }
-                });
-            } else {
-                return null
-            }
+      return tagsObject;
+    },
+  };
 
-            return tagsObject
+  /**
+   * Check to see if we have a file with save data already. If we do, then it
+   * gets passed to the sendFile channel as the parameter data (see the main.js in the root of the project).
+   * If not then data is false/null
+   *  */
 
-        }
+  window.api.send("loadFile");
+
+  // Create ink story from the content using inkjs
+  var story = new inkjs.Story(storyContent);
+
+  // Create an event listener for the save button so the user can save the state there in.
+  document.getElementById("save").addEventListener("click", () => {
+    let savedJson = story.state.ToJson();
+    window.api.send("saveFile", { name: "currentSave.json", data: savedJson });
+  });
+
+  /** ---------------------------------------
+   *  Variable Observables
+   * ----------------------------------------
+   * Every time a variable within ink is changed the observable function fires off
+   */
+  story.ObserveVariable("weapon", (varName, newValue) => {
+    console.log(newValue);
+  });
+
+  /** ---------------------------------------
+   *  Global Tags
+   * ----------------------------------------
+   * Any tags provided at the very top of the main ink file are accessible via the Story's globalTags property,
+   * which also returns a List<string>. Any top level story metadata can be included there.
+   */
+  const globalTags = helper.parseGlobalTags(story.globalTags);
+
+  /** ---------------------------------------
+   *  Grab Dom Elements for appending story elements
+   * ----------------------------------------
+   */
+  const storyWrapperDiv = document.getElementById("story-wrapper");
+  const questionWrapperDiv = document.getElementById("question-wrapper");
+
+  window.api.receive("sendFile", (data) => {
+    //  if (data) {
+    //      story.state.LoadJson(JSON.stringify(data));
+    //  }
+
+    continueStory();
+
+    function continueStory() {
+      while (story.canContinue) {
+        let currentStoryLine = story.Continue();
+        let paragraph = document.createElement("p");
+        let lineText = document.createTextNode(currentStoryLine);
+
+        paragraph.appendChild(lineText);
+        storyWrapperDiv.append(paragraph);
+      }
+
+      if (story.currentChoices.length > 0) {
+        story.currentChoices.forEach((choice) => {
+          console.log(choice);
+
+          let anchor = document.createElement("a");
+          anchor.href = "#";
+          let questionText = document.createTextNode(choice.text);
+
+          anchor.appendChild(questionText);
+          questionWrapperDiv.append(anchor);
+
+          anchor.addEventListener("click", function (event) {
+            // Don't follow <a> link
+            event.preventDefault();
+
+            storyWrapperDiv.innerHTML = "";
+            questionWrapperDiv.innerHTML = "";
+
+            // Tell the story where to go next
+            story.ChooseChoiceIndex(choice.index);
+
+            // Aaand loop
+            continueStory();
+          });
+        });
+      }
     }
 
-    /**
-     * Check to see if we have a file with save data already. If we do, then it
-     * gets passed to the sendFile channel as the parameter data (see the main.js in the root of the project).
-     * If not then data is false/null
-     *  */ 
-    window.api.send("loadFile");
-
-    // Create ink story from the content using inkjs
-    var story = new inkjs.Story(storyContent);
-
-    // Create an event listener for the save button so the user can save the state there in.
-    document.getElementById("save").addEventListener("click", () => {
-        let savedJson = story.state.ToJson();
-        window.api.send("saveFile", {name: "currentSave.json", data: savedJson});
-    });
-
-    /** ---------------------------------------
-     *  Variable Observables
-     * ----------------------------------------
-     * Everytime a variable within ink is changed the observable function fires off
-     */
-    story.ObserveVariable("sanity", (varName, newValue) => {
-        console.log(newValue)
-    });
-
-    /** ---------------------------------------
-     *  Global Tags
-     * ----------------------------------------
-     * Any tags provided at the very top of the main ink file are accessible via the Story's globalTags property, 
-     * which also returns a List<string>. Any top level story metadata can be included there.
-     */
-    console.log(story.globalTags)
-    const globalTags = helper.parseGlobalTags(story.globalTags);
-    console.log(globalTags);
-
-    window.api.receive("sendFile", (data) => {
-
-      //  if (data) {
-      //      story.state.LoadJson(JSON.stringify(data));
-      //  }
-
-        
-
-    });
-
+  });
 })(storyContent);
